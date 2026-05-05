0e42a1594f337be2e902c25d742e45f57de0d9aa
SymonML <156556884+SymonML@users.noreply.github.com>
2026-05-05 17:39:16 -0300

    Atividade-POO2-Java-Swing

---
+++ src/main/java/com/appcadastro/labthreadsswing/LabThreadsSwing.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.appcadastro.labthreadsswing;
+
+/**
+ *
+ * @author User
+ */
+public class LabThreadsSwing {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
